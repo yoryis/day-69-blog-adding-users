7d2091ebe7cc175e1ee3e6e243bb30a0492a3fb0
Michel Navarro <michel.navarro@ifsworld.com>
2022-06-11 15:43:06 +0100

    Completed Blog Tool 0.2

--- server.py
+++ server.py
@@ -11,19 +11,20 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 
 # Admin user id is user 1
-ADMIN_USER_ID = 1
+# ADMIN_USER_ID = 1
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 # To add HTML editor to the page in make-post.html
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
